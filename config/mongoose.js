380eafa4ecc256a672c77f49d5ecbb93182b7ba5
Ashvarya <94183306+Ashvarya@users.noreply.github.com>
2023-09-17 18:13:19 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1/Hospital1_Api');
+mongoose.connect('mongodb://127.0.0.1:27017/Hospital1_Api');
 const db = mongoose.connection;
 
 db.on('error',console.error.bind(console,'Error connecting to the db'));
